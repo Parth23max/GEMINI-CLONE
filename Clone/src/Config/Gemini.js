91ddf6cdd13adea9ddb916d7e61356ea0f21bd45
PARTH <its.parth.k@gmail.com>
2024-12-19 23:23:13 +0530

    updated api key

--- Clone/src/Config/Gemini.js
+++ Clone/src/Config/Gemini.js
@@ -6,7 +6,7 @@ import {
 
 
 
-const apiKey = "YOUR_API_KEY"
+const apiKey = "AIzaSyAh7benOR6zFWN8z6JyCgAZkkW69PkfWXE";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
